52f4b4d593253c79624dbced215117a186ac90cc
itsarnavsh <its.arnav.sh@gmail.com>
2024-04-15 22:32:34 +0530

    Contents

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,7 @@
 <script>
     import INTRO from "./intro/+page.svelte"
+    import Contents from "./contents/+page.svelte"
 </script>
 <INTRO />
+<br>
+<Contents />
---
+++ src/routes/contents/+page.svelte
@@ -0,0 +1,21 @@
+<style>
+    .backgrounder {
+      background-image: url('/img/toc.png');
+      background-repeat: no-repeat;
+      background-size: cover;
+      height:100vh;
+    }
+</style>
+<script>
+    let contents = ["Dining Experience","Elevated Dining Experience at Udaivilas","Experiences","Shopping Options","Sightseeing Options","The Oberoi Spa"];
+</script>
+<div class = "backgrounder">
+    <div class = "text-3xl titlefont-regular p-5 pt-40 text-center">
+        Navigating Your Stay
+    </div>
+    <div class = "text-center">
+    {#each contents as content}
+    <a href = "/"><p class = "text-black content-regular m-10 text-xl ">{content}</p></a>
+    {/each}
+    </div>
+</div>
