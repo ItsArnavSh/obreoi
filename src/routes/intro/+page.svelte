7c7ca1fdcdb0dfa3644e613098cea3598e64b136
itsarnavsh <its.arnav.sh@gmail.com>
2024-04-15 21:28:28 +0530

    Viewport

--- src/routes/intro/+page.svelte
+++ src/routes/intro/+page.svelte
@@ -6,20 +6,20 @@
       background-image: url('/img/title.png');
       background-repeat: no-repeat;
       background-size: cover;
-      height:900px;
+      height:100vh;
     }
     </style>
   <div class = "intro">
         <div class = "text-center flex flex-col justify-center">
             <h1 class = "text-3xl titlefont-bold text-white mt-20">
-                The Spirit of <br>The Obreoi Udaivilas
+                The Spirit of <br>The Oberoi Udaivilas
             </h1>
             
-            <div class = "content text-white text-sm p-10 text-container">
+            <div class = "content text-white text-lg pb-10 pt-10 text-container">
                 <p class = "pb-4">
                     Nestled amidst the majestic Aravalli Mountain Range, The Oberoi Udaivilas stands as a mesmerizing tribute to the opulent legacy of Mewar. Our sanctuary exudes an aura of tranquility and romance, promising to immerse you in an unforgettable journey of splendor.
                 </p>
-                <p>
+                <p class = "pt-40 mt-20">
                     With meticulous attention to detail and heartfelt hospitality, we strive to craft an experience that transcends ordinary luxury. Each moment spent with us is infused with warmth and sincerity, destined to weave a tapestry of happiness that lingers in your memory.
                 </p>
             </div>
